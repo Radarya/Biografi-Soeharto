c366d6995bde6199138207f262a0bab4023e7dc1
Radithya Aydin Aryasatya <aydinaryasatyaradithya@gmail.com>
2025-02-14 21:15:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll("[data-scroll-to]").forEach(function (element) {
+        element.addEventListener("click", function () {
+            const targetId = this.getAttribute("data-scroll-to");
+            const targetElement = document.getElementById(targetId);
+            
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: "smooth", block: "start" });
+            }
+        });
+    });
+
+    var btn = $('#button');
+
+    $(window).scroll(function() {
+        console.log("Scrolling...");
+        if ($(window).scrollTop() > 300) {
+            btn.addClass('show');
+        } else {
+            btn.removeClass('show');
+        }
+    });
+
+    btn.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop: 0}, '300');
+    });
+
+    console.log("jQuery is loaded");
+});
